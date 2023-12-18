ae3d3212f97363b71b7456f7f34ff047a4c230d5
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:26:53 +0200

    Adding code examples/adidas_curl.sh

---
+++ code examples/adidas_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.adidas.com/us/under_100-shoes"}'
